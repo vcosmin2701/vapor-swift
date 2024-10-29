f11a2f811d091fda35c2079eeb7dd8c89db8cf39
vcosmin2701 <v.cosmin2717@gmail.com>
2024-10-29 15:45:53 +0200

    GET Request
    Introduction to Vapor
    
    Basic example of a GET request and a request with a parameter

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -8,4 +8,13 @@ func routes(_ app: Application) throws {
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
+    
+    app.get("hello", "vapor") { req in
+        "Hello, Vapor!!"
+    }
+    
+    app.get("hello", ":name") { req -> String in
+        let name = try req.parameters.require("name", as: String.self)
+        return "Hello \(name)"
+    }
 }
